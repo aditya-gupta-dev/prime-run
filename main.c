cff5de952ed6990140d7d03acca15033a51417a3
Aditya Gupta <adityagupta2087@gmail.com>
2026-02-22 17:06:57 +0530

    completed

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include<stdio.h> 
-#include<stdlib.h> 
+#include <stdlib.h>
 #include<unistd.h>
 
 int main(int argc, char* argv[]) {
@@ -7,16 +7,24 @@ int main(int argc, char* argv[]) {
 		printf("\tprime-run\nrun your programs on nvidia gpu.\n");
 	        return 0;	
 	} 
-	
-	char *env[] = { 
-		"__NV_PRIME_RENDER_OFFLOAD=1",
-		"__GLX_VENDOR_LIBRARY_NAME=nvidia",
-		"__VK_LAYER_NV_optimus=NVIDIA_only" 
-	};  
 
-	char **proc = &argv[1]; 
+  pid_t pid = fork(); 
 
-	execve(proc[0], proc, env);
+  if (pid < 0) { 
+    perror("failed to fork"); 
+    return 1; 
+  }
 
+  if (pid == 0) { 
+    setenv("__NV_PRIME_RENDER_OFFLOAD", "1", 1); 
+    setenv("__GLX_VENDOR_LIBRARY_NAME", "nvidia", 1);
+
+    char* args[] = { argv[1], NULL };
+    execvp(args[0], args);
+
+    perror("failed to launch program."); 
+  } else { 
+    printf("exiting..."); 
+  }
 	return 0; 
 } 
